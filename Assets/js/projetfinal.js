500bd9360c9323f942398f2a169a3120b32d5a89
William John Adam Trindade <wjat.joi@gmail.com>
2021-03-31 22:33:20 -0400

    Fix bug validadrion

--- Assets/js/projetfinal.js
+++ Assets/js/projetfinal.js
@@ -18,15 +18,121 @@ recalculateRecette1 = function () {
 login = function () {
   var email = document.getElementById("UserEmail").value;
   var pwd = document.getElementById("UserPwd").value;
+  var regexPWD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+  var regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
+  if (!regexEmail.test(email)) {
+    document.getElementById("UserEmail").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("UserEmail").style.color = "black";
+  }
+
+  if (!regexPWD.test(pwd)) {
+    document.getElementById("UserPwd").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("UserPwd").style.color = "black";
+  }
+  alert("Bienvenue " + email);
   console.log(email, pwd);
 };
 sendContact = function () {
   var name = document.getElementById("ContactName").value;
   var email = document.getElementById("ContactEmail").value;
   var message = document.getElementById("Message").value;
+  var regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!regexEmail.test(email)) {
+    document.getElementById("ContactEmail").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("ContactEmail").style.color = "black";
+  }
+  alert("Message envoyé avec sucess!");
 
   console.log(name, email, message);
 };
 
-incription = function () {};
+incription = function () {
+  var nom = document.getElementById("Nom").value;
+  var prenom = document.getElementById("Prenom").value;
+
+  var email1 = document.getElementById("UserEmail").value;
+  var email2 = document.getElementById("UserEmailConfirmation").value;
+
+  var pwd1 = document.getElementById("UserPwd1").value;
+  var pwd2 = document.getElementById("UserPwd2").value;
+
+  var GenreEl = document.querySelector('input[name="Genre"]:checked');
+  var VilleEl = document.getElementById("Ville");
+  var Ville = VilleEl.options[VilleEl.selectedIndex].text;
+
+  $(".dropdown").each(function (key, dropdown) {
+    var $dropdown = $(dropdown);
+    $dropdown.find(".dropdown-menu a").on("click", function () {
+      $dropdown.find("button").text($(this).text()).append(' <span class="caret"></span>');
+      $("#dropdown_input").val($(this).text()); // change the value of hidden input field
+    });
+  });
+
+  var regexPWD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+  var regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!regexEmail.test(email1)) {
+    document.getElementById("UserEmail").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("UserEmail").style.color = "black";
+  }
+
+  if (!regexEmail.test(email2)) {
+    document.getElementById("UserEmailConfirmation").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("UserEmailConfirmation").style.color = "black";
+  }
+
+  if (email1 !== email2) {
+    document.getElementById("UserEmailConfirmation").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("UserEmailConfirmation").style.color = "black";
+  }
+
+  if (!regexPWD.test(pwd1)) {
+    document.getElementById("UserPwd1").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("UserPwd1").style.color = "black";
+  }
+
+  if (!regexPWD.test(pwd2)) {
+    document.getElementById("UserPwd2").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("UserPwd2").style.color = "black";
+  }
+
+  if (pwd1 !== pwd2) {
+    document.getElementById("UserPwd2").style.color = "red";
+    return false;
+  } else {
+    document.getElementById("UserPwd2").style.color = "black";
+  }
+  //verifier si le genre est selectionné
+  if (!GenreEl) {
+    document.getElementById("genreerror").style.visibility = "visible";
+    return false;
+  } else {
+    document.getElementById("genreerror").style.visibility = "hidden";
+    var Genre = GenreEl.value;
+  }
+  if (!Ville) {
+    document.getElementById("villeerror").style.visibility = "visible";
+    return false;
+  } else {
+    document.getElementById("villeerror").style.visibility = "hidden";
+  }
+
+  alert("Inscription fait avec success!");
+};
